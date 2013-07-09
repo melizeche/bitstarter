a9c2c99292088cd2251fcbf0ae768104699753cd
Marcelo Elizeche <melizeche@gmail.com>
2013-07-09 06:40:42 +0000

    web.js mod4

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
+ 
 var ff = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
-  response.send("afgfdg");
+  response.send(ff.toString());
 });
 
 var port = process.env.PORT || 5000;
